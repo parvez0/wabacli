de84adca928ff67bb46eb2077ec65d76c202a569
parvez <syedparvez72@gmail.com>
2021-03-27 23:33:05 +0530

    updated install.sh file

--- install.sh
+++ install.sh
@@ -5,11 +5,9 @@
 #
 # Other options:
 #   --installation_path                     where to install executable default to /usr/bin
-#   --update                                update the installed version to latest release
 #   --version                               install a specific version (eg. v1.2.3)
-#   --http_agent                            which tool utility to use curl or wget, by defaults it will search for available tool
 #
-# shellcheck disable=SC2039,SC2059,SC2086,SC2046,SC2006
+# shellcheck disable=SC2039,SC2059,SC2086,SC2046,SC2006,SC2120
 # =================== loggers ==================
 
 RETRIES=1
@@ -44,10 +42,11 @@ warning() {
 AGENT=""
 OPTIONS=""
 INTERACTIVE=1
-UPDATE=0
 VERSION="latest"
 ARCH=""
+INSTTALLATION_PATH="/usr/bin/"
 
+# verifying dependencies
 verify() {
   info "verifying dependencies required for setup"
   if [ `command -v curl` ]; then
@@ -61,6 +60,19 @@ verify() {
   fi
 }
 
+# fetching the system info for architecture details
+system_info() {
+  SYSTEM="$(uname -s 2> /dev/null || uname -v)"
+  OS="$(uname -o 2> /dev/null || uname -rs)"
+  MACHINE="$(uname -m 2> /dev/null)"
+  ARCH=$(echo "${SYSTEM}_${MACHINE}" | tr '[:upper:]' '[:lower:]')
+
+  info "SYSTEM       :   $SYSTEM"
+  info "MACHINE      :   $MACHINE"
+  info "ARCHITECTURE :   $ARCH"
+}
+
+# get the latest release version from the server
 latest_release() {
   URL="https://raw.githubusercontent.com/parvez0/wabacli/main/.release"
   if [ $VERSION == "latest" ]; then
@@ -76,18 +88,58 @@ latest_release() {
   fi
 }
 
-system_info() {
-  SYSTEM="$(uname -s 2> /dev/null || uname -v)"
-  OS="$(uname -o 2> /dev/null || uname -rs)"
-  MACHINE="$(uname -m 2> /dev/null)"
-
-
-
-}
-
+# download the latest released binaries for installation
 download_tarball(){
-  URL=""
+  URL="https://github.com/parvez0/wabacli/releases/download/$VERSION/wabacli_$VERSION_$ARCH.tar.gz"
+  info "downloading latest release binaries from $URL"
+  cmd="$AGENT -O wabacli_$VERSION_$ARCH.tar.gz $URL"
+  eval "$cmd"
+  echo "extracting tar ball to wabacli_$VERSION_$ARCH"
+  tar -C "wabacli_$VERSION_$ARCH" -xf "wabacli_$VERSION_$ARCH.tar.gz"
 }
 
+# copy the binary to installation path
+install(){
+  echo "installing wabacli"
+  sudo cp "/tmp/wabacli_$VERSION_$ARCH/wabacli" $INSTTALLATION_PATH
+}
+
+#verify
+#latest_release
+#system_info
+
+# ----------------------------------------------------------------------------------
+validate_input() {
+    if [[ $2 == "" || $2 == --* ]]; then
+        fatal "$1 requires input"
+    fi
+}
+
+while [ -n "${1}" ]; do
+    arg="$1"
+    case "${1}" in
+    "--installation_path")
+        shift 1
+        INSTTALLATION_PATH="${1}"
+        validate_input "$arg" "$1"
+        if [ ! -d "$1" ]; then
+          fatal "specified directory \"$1\" does not exits"
+        fi
+        info "setting up installation path to $INSTTALLATION_PATH"
+        ;;
+    "--version")
+        shift 1
+        VERSION="${1}"
+        validate_input "$arg" "$1"
+        info "installing application with version \"$VERSION\""
+        ;;
+    esac
+    shift 1
+done
+
+# ------------------- #
 verify
+system_info
 latest_release
+download_tarball
+install
